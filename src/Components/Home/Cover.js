5a408829c57c35d87b18f582be235261cb5909e5
getch-hasan <hasan15-3112@diu.edu.bd>
2023-06-07 00:29:21 +0600

    after add search and filter product by search

---
+++ src/Components/Home/Cover.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import img1 from '../../image/original.webp'
+import img2 from '../../image/Merch-By-Amazon-Feature-Image-Post-One-Tyler-Bryden.png'
+import img3 from '../../image/coverimage.avif'
+
+const Cover = () => {
+    return (
+        <div class="carousel w-full">
+            
+            <div id="slide2" class="carousel-item relative w-full">
+                <img src={img1} class="w-full" />
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" class="btn btn-circle">❮</a>
+                    <a href="#slide3" class="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" class="carousel-item relative w-full">
+                <img src={img3} class="w-full" />
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" class="btn btn-circle">❮</a>
+                    <a href="#slide4" class="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" class="carousel-item relative w-full">
+                <img src={img2} class="w-full" />
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" class="btn btn-circle">❮</a>
+                    <a href="#slide1" class="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Cover;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,22 +3,60 @@ import React, { useEffect, useState } from 'react';
 import Product from './Product';
 import Footer from '../shared/Footer';
 
+
 const Home = () => {
     const [products, setProducts] = useState([])
+    const [searchQuery, setSearchQuery] = useState('');
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
         fetch('http://localhost:8000/product')
             .then(res => res.json())
-            .then(data => setProducts(data))
+            .then(data => {
+                setProducts(data)
+                setFilteredProducts(data)
+                setIsLoading(false)
+            })
     }, [])
+    if (isLoading) {
+        return  <span class="loading s loading-ring loading-lg"></span>
+        
+    }
+
+    const handleSearch = (e) => {
+
+
+        const filtered = products.filter((product) =>
+            product.category.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            product.seller.toLowerCase().includes(searchQuery.toLowerCase())
+
+
+        );
+        setFilteredProducts(filtered)
+        setSearchQuery('')
+
+    };
+
 
 
     return (
         <div>
+            <div className='flex mb-5 justify-center items-center '>
+                <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} name='search' type="text" placeholder="search here" class="input input-bordered input-info w-full max-w-xs " />
+                <button onClick={() => handleSearch()} className='btn btn-primary ms-4'>Search</button>
+            </div>
+
+
 
             <div className='ms-7  grid gap-4 lg:grid-cols-3  grid-rows-3 sm:grid-cols-1'>
 
+
+
                 {
-                    products.map(product => <Product
+
+
+
+                    filteredProducts.map(product => <Product
                         product={product} ></Product>)
                 }
             </div>
